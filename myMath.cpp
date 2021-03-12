c9f5d0e63ea5d0dbd75880cf5e354ce12f9cc004
Alex <Alex.pevnenko@gmail.com>
2021-03-12 21:39:00 +0300

    добавил функцию НОД (определение наибольшего общего делителя)

---
+++ myMath.cpp
@@ -0,0 +1,25 @@
+﻿#include "myMath.h"
+
+uint32_t gcd(int32_t x, int32_t y) 
+{
+  uint32_t result=1;
+  if (x < 0) x*=(-1);
+  if (y < 0) y*=(-1);
+  if (x==0||y==0) return 0;
+  
+  if (x%y==0) 
+  {
+    if (x >= y)
+      return y;
+    else
+      return y;
+  }
+
+  while (x % y != 0) 
+  {
+    result = x % y;
+    x = y;
+    y = result;
+  }
+  return result;
+}
---
+++ myMath.h
@@ -0,0 +1,9 @@
+#pragma once 
+/**
+ * @brief
+ * This function finds the greatest common divisor
+ * @param x: first number
+ * @param y: second number
+ * @return function result - greatest common divisor
+ */
+uint32_t gcd(int32_t , int32_t);
